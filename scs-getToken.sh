f9e347f9984d3ed2e0de14f97b2943e59833dcef
shinyay <shinya.com@gmail.com>
2017-06-21 21:32:29 +0900

    Modify it abide by my script template

--- scs-getToken.sh
+++ scs-getToken.sh
@@ -1,11 +1,76 @@
 #!/bin/sh
-if [ $# -ne 3 ]; then
-  echo "COMMAND <IDENTITY DOMAIN> <STORAGE USER> <STORAGE PASSWORD>"
+
+#!/bin/sh
+
+# ##################################################
+#
+version="1.0.0"
+#
+# HISTORY:
+#
+# * 17/06/21 - v1.0.0  - First Creation
+#
+# ##################################################
+
+function mainScript() {
+  curl -v -X GET -H "X-Storage-User: Storage-${IDDOMAIN}:${USERID}" -H "X-Storage-Pass: ${PASSWORD}" https://${DATACENTER}.storage.oraclecloud.com/auth/v1.0
+}
+
+function usage() {
+    cat <<EOF
+$(basename ${0}) is a tool for ...
+Usage:
+    $(basename ${0}) -i <IDENTITYDOMAIN> -u <USERNAME> -p <PASSWORD> -c <DATACENTER>
+Options:
+    DATACENTER     print us2|em2
+EOF
+}
+
+# Check Arguments
+if [ $# -eq 0 ]; then
+  usage
   exit 1
 fi
 
-IDDOMAIN=$1
-STORAGE_USER=$2
-STORAGE_PWD=$3
-echo "curl -v -X GET -H "X-Storage-User: Storage-${IDDOMAIN}:${STORAGE_USER}" -H "X-Storage-Pass: ${STORAGE_PWD}" https://${IDDOMAIN}.storage.oraclecloud.com/auth/v1.0"
-curl -v -X GET -H "X-Storage-User: Storage-${IDDOMAIN}:${STORAGE_USER}" -H "X-Storage-Pass: ${STORAGE_PWD}" https://${IDDOMAIN}.storage.oraclecloud.com/auth/v1.0
+# Handle Options
+while [ $# -gt 0 ];
+do
+    case ${1} in
+
+        --debug|-d)
+            set -x
+        ;;
+
+        --version|-v)
+            echo "$(basename ${0}) ${version}"
+            exit 0
+        ;;
+
+        --iddomain|-i)
+            IDDOMAIN=${2}
+            shift
+        ;;
+
+        --user|-u)
+            USERID=${2}
+            shift
+        ;;
+
+        --password|-p)
+            PASSWORD=${2}
+            shift
+        ;;
+        --datacenter|-c)
+            DATACENTER=${2}
+	    shift
+	;;
+        *)
+            echo "[ERROR] Invalid option '${1}'"
+            usage
+            exit 1
+        ;;
+    esac
+    shift
+done
+
+mainScript
